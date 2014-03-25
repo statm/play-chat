301c91bebf279729de42d98cada685c41805262e
R Zhao <arcus.zhao@gmail.com>
2014-03-25 13:52:22 -0700

    model

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,13 +4,28 @@ import play.*;
 import play.mvc.*;
 
 import java.util.*;
+import java.util.concurrent.ExecutionException;
 
 import models.*;
+import models.NotificationHost.Notification;
+import models.NotificationHost.Chat;
 
 public class Application extends Controller {
 
     public static void index() {
         render();
     }
-
+    
+    public static void getN() throws InterruptedException, ExecutionException {
+    	Object[] notifications = await(NotificationHost.get().stream.getNotifications());
+    	renderJSON(notifications);
+    }
+    
+    public static void addN() {
+    	NotificationHost.get().stream.addEvent(new Notification("Notification at " + new Date().toString()));
+    }
+    
+    public static void addC() {
+    	NotificationHost.get().stream.addEvent(new Chat("Chat at " + new Date().toString()));
+    }
 }
---
+++ app/models/NotificationHost.java
@@ -0,0 +1,121 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import play.libs.F.*;
+
+public class NotificationHost {
+
+	public static final int EVENT_POLL_INTERVAL = 10000;
+	public static final int CHAT_POLL_INTERVAL = 500;
+
+	public EventStream stream;
+
+	private NotificationHost() {
+		stream = new EventStream();
+	}
+
+	// stream
+	public static class EventStream {
+		private static Timer pollIntervalTimer = new Timer("EventStream.Timer", true);
+		private static TimerTask pollIntervalTimerTask;
+		private Long lastPublishTime = 0L;
+
+		private ConcurrentLinkedQueue<Event> events = new ConcurrentLinkedQueue<Event>();
+		private List<Promise<Event[]>> waiting = Collections.synchronizedList(new ArrayList<Promise<Event[]>>());
+
+		public void addEvent(Event event) {
+			events.offer(event);
+			
+			Long currentTime = System.currentTimeMillis();
+			
+			if (currentTime - lastPublishTime > EVENT_POLL_INTERVAL
+				|| event.type == EventType.Chat) {
+				publishEvents();
+			} else {
+				if (pollIntervalTimerTask != null) {
+					pollIntervalTimerTask.cancel();
+				}
+				
+				pollIntervalTimerTask = new TimerTask() {
+
+					@Override
+					public void run() {
+						publishEvents();
+					}
+				};
+				
+				pollIntervalTimer.schedule(pollIntervalTimerTask, lastPublishTime + EVENT_POLL_INTERVAL - currentTime);
+			}
+		}
+
+		public synchronized Promise<Event[]> getNotifications() {
+			Promise<Event[]> promise = new Promise<Event[]>();
+			waiting.add(promise);
+			return promise;
+		}
+
+		private void publishEvents() {
+			if (waiting.size() == 0) {
+				return;
+			}
+
+			Event[] array = new Event[events.size()];
+			for (Promise<Event[]> promise : waiting) {
+				promise.invoke(events.toArray(array));
+			}
+			events.clear();
+			waiting.clear();
+			lastPublishTime = System.currentTimeMillis();
+			if (pollIntervalTimerTask != null) {
+				pollIntervalTimerTask.cancel();
+				pollIntervalTimerTask = null;
+			}
+		}
+	}
+
+	// events
+	private static enum EventType {
+		Notification, Chat
+	}
+
+	public static abstract class Event {
+		public String text;
+		public Long time;
+		public EventType type;
+
+		public Event(String text) {
+			this.text = text;
+			this.time = System.currentTimeMillis();
+		}
+	}
+
+	public static class Notification extends Event {
+		public Notification(String text) {
+			super(text);
+			this.type = EventType.Notification;
+		}
+	}
+
+	public static class Chat extends Event {
+		public Chat(String text) {
+			super(text);
+			this.type = EventType.Chat;
+		}
+	}
+
+	// singleton
+	private static NotificationHost instance = null;
+
+	public static NotificationHost get() {
+		if (instance == null) {
+			instance = new NotificationHost();
+		}
+		return instance;
+	}
+}
